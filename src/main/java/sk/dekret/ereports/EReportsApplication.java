3f6f4aca39c0fd32136e054f336e2618660c2ed1
Simon Dekret <sdekret@gmail.com>
2023-05-13 17:39:41 +0200

    add empty mvn project

---
+++ src/main/java/sk/dekret/ereports/EReportsApplication.java
@@ -0,0 +1,13 @@
+package sk.dekret.ereports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EReportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EReportsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/dekret/ereports/EReportsApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.dekret.ereports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EReportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
