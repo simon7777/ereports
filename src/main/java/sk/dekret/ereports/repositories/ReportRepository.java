bdf23032bfd3c7f91e68185e80efd71262487831
Simon Dekret <sdekret@gmail.com>
2023-05-16 15:28:50 +0200

    Add ReportRepository

--- src/main/java/sk/dekret/ereports/repositories/ReportRepository.java
+++ src/main/java/sk/dekret/ereports/repositories/ReportRepository.java
@@ -3,5 +3,8 @@ package sk.dekret.ereports.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sk.dekret.ereports.db.entities.Report;
 
+import java.util.List;
+
 public interface ReportRepository extends JpaRepository<Report, Long> {
+    List<Report> findAllByUserAccountId(Long userAccountId);
 }
